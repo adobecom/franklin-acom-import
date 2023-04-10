1fd3e703dcdd87c7353d10b4c37e5c146d8ccf03
mathuria <mathuria@adobe.com>
2023-04-11 00:19:50 +0530

    Fixing block issues

--- tools/importer/rules/promoColumn.js
+++ tools/importer/rules/promoColumn.js
@@ -19,6 +19,21 @@ export default function createPromoColumn(block, document) {
   const table = WebImporter.DOMUtils.createTable(cells, document);
   table.classList.add('import-table');
 
+  const sectionMetadata = [['section-metadata']];
+  const bgColor = block.querySelector('div[data-bgcolor]')?.dataset.bgcolor;
+  const bgImage = block.querySelector('div[style]')?.getAttribute('style').split('"')[1];
+  if (bgImage) {
+    const bgImageElement = document.createElement('img');
+    bgImageElement.src = bgImage;
+    sectionMetadata.push([['style'], ['dark, xl spacing']]);
+    sectionMetadata.push(['background', bgImageElement]);
+  } else if (bgColor) {
+    sectionMetadata.push(['background', block.querySelector('div[data-bgcolor]')?.dataset.bgcolor]);
+  }
+  const sectionMetadataTable = WebImporter.DOMUtils.createTable(sectionMetadata, document);
+  sectionMetadataTable.classList.add('import-table');
+
   block.before(document.createElement('hr'));
   block.replaceWith(...block.querySelectorAll('.import-table'), table);
+  table.after(sectionMetadataTable);
 }
