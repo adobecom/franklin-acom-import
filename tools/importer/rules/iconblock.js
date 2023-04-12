35704ab811374c18450e7f4dbe519a149ef074e2
glo77801 <glo77801@adobe.com>
2023-04-12 22:09:27 +0530

    Updated import script for some blocks

--- tools/importer/rules/iconblock.js
+++ tools/importer/rules/iconblock.js
@@ -1,187 +1,37 @@
 /* global WebImporter */
-const hasClass = (element, className) => !!(element && element.classList.contains(className));
 
 const creativityForAllIconBlock = (block, document) => {
-  const iconBlockCreation = (container, type) => {
-    let creativityContent = container;
-    if (type === 'Pattern1') {
-      creativityContent = container.querySelector(
-        '.position .dexter-Position > div',
-      );
-    }
-
-    // find image
-    const imageElement = creativityContent.querySelector('.image img');
-    let imageSrc = '';
-    if (imageElement?.getAttribute('src')?.indexOf('https') === -1) {
-      imageSrc = `https://www.adobe.com${imageElement.getAttribute('src')}`;
-    } else {
-      imageSrc = imageElement.getAttribute('src');
-    }
-    const imageLink = document.createElement('img');
-    imageLink.setAttribute('src', imageSrc);
-
-    // find title content
-    let titleElement = creativityContent.querySelector('.title h2');
-    if (!titleElement) {
-      titleElement = creativityContent.querySelector('.title h3');
-    }
-    if (!titleElement) {
-      titleElement = creativityContent.querySelector('.text h1');
-    }
-    if (!titleElement) {
-      titleElement = creativityContent.querySelector('.text h2');
-    }
-
-    // find description element
-    const descriptionElement = creativityContent.querySelectorAll('.text p');
-
-    // find link element
-    const linkElement = creativityContent.querySelector('.text a');
-
-    if (imageLink && titleElement && descriptionElement.length && linkElement) {
-      const contentCell = document.createElement('div');
-      contentCell.appendChild(imageLink);
-      contentCell.appendChild(titleElement);
-      descriptionElement.forEach((element) => {
-        contentCell.appendChild(element);
-      });
-      contentCell.appendChild(linkElement);
-      const cells = [['icon-block (fullwidth, large)'], [contentCell]];
-
-      // icon block table
-      const table = WebImporter.DOMUtils.createTable(cells, document);
-      table.classList.add('import-table');
-      block.before(document.createElement('hr'));
-      const sectionMetadataCells = [
-        ['Section Metadata'],
-        ['style', 'xxxl spacing'],
-      ];
-      // section metadata Table
-      const sectionMetaDataTable = WebImporter.DOMUtils.createTable(
-        sectionMetadataCells,
-        document,
-      );
-      sectionMetaDataTable.classList.add('import-table');
-      block.replaceWith(table, sectionMetaDataTable);
-    } else {
-      const contentCell = document.createElement('div');
-      if (imageLink) {
-        contentCell.appendChild(imageLink);
-      }
-      if (titleElement) {
-        contentCell.appendChild(titleElement);
-      }
-      descriptionElement.forEach((element) => {
-        contentCell.appendChild(element);
-      });
-      if (linkElement) {
-        contentCell.appendChild(linkElement);
-      }
-      const cells = [['icon-block (fullwidth, large)'], [contentCell]];
-      // icon block table
-      const table = WebImporter.DOMUtils.createTable(cells, document);
-      table.classList.add('import-table');
-      block.before(document.createElement('hr'));
-      const sectionMetadataCells = [
-        ['Section Metadata'],
-        ['style', 'xxxl spacing'],
-      ];
-      // section metadata Table
-      const sectionMetaDataTable = WebImporter.DOMUtils.createTable(
-        sectionMetadataCells,
-        document,
-      );
-      sectionMetaDataTable.classList.add('import-table');
-      block.replaceWith(table, sectionMetaDataTable);
-    }
-  };
-
-  const checkPattern1 = (container) => {
-    const creativityContent = container.querySelector(
-      '.position .dexter-Position > div',
-    );
-    const childCountValidation = creativityContent.childElementCount === 4;
-    const childElements = ['image', 'title', 'text', 'text'];
-    let testChildOrder = true;
-    if (childCountValidation) {
-      childElements.forEach((element, index) => {
-        if (!creativityContent.children[index].classList.contains(element)) {
-          testChildOrder = false;
-        }
-      });
-    }
-    return childCountValidation && testChildOrder;
-  };
-
-  const checkPattern2 = (container) => {
-    const creativityContent = container;
-    const childElements = ['image', 'text', 'text', 'text'];
-    let testChildOrder = true;
-    childElements.forEach((element, index) => {
-      if (!creativityContent.children[index].classList.contains(element)) {
-        testChildOrder = false;
-      }
-    });
-    return testChildOrder;
-  };
-
-  const checkPattern3 = (container) => {
-    const creativityContent = container;
-    const childElements = ['image', 'title', 'text', 'text'];
-    let testChildOrder = true;
-    childElements.forEach((element, index) => {
-      if (!creativityContent.children[index].classList.contains(element)) {
-        testChildOrder = false;
-      }
-    });
-    return testChildOrder;
-  };
-
-  const checkPattern4 = (container) => {
-    const creativityContent = container.querySelector(
-      '.position .dexter-Position > div',
-    );
-    const childCountValidation = creativityContent.childElementCount === 3;
-    const childElements = ['image', 'text', 'text'];
-    let testChildOrder = true;
-    if (childCountValidation) {
-      childElements.forEach((element, index) => {
-        if (!creativityContent.children[index].classList.contains(element)) {
-          testChildOrder = false;
-        }
-      });
-    }
-    return childCountValidation && testChildOrder;
-  };
-
-  const containers = [];
-  [...block.querySelectorAll('.dexter-FlexContainer-Items')].forEach(
-    (c) => {
-      const order1 = c.childElementCount === 1
-      && hasClass(c.children[0], 'position')
-      && (checkPattern1(c) || checkPattern4(c));
-      const order2 = c.childElementCount === 4
-      && hasClass(c.children[0], 'image')
-      && hasClass(c.children[0], 'parbase')
-      && (checkPattern2(c) || checkPattern3(c));
-      if (order1) {
-        containers.push({
-          element: c,
-          type: 'Pattern1',
-        });
-      } else if (order2) {
-        containers.push({
-          element: c,
-          type: 'Pattern2',
-        });
-      }
-    },
+  // background color or background image
+  const bgImage = block
+    .querySelector('div[style]')
+    ?.getAttribute('style')
+    .split('"')[1];
+  const bgcolor = block
+    .querySelector('div[data-bgcolor]')
+    ?.getAttribute('data-bgcolor');
+  let bgImageElement = null;
+  if (bgImage) {
+    bgImageElement = document.createElement('img');
+    bgImageElement.src = bgImage;
+  }
+  const cells = [['icon-block (fullwidth, large)'], [block.cloneNode(true)]];
+  const table = WebImporter.DOMUtils.createTable(cells, document);
+  table.classList.add('import-table');
+  block.before(document.createElement('hr'));
+  const sectionMetadataCells = [
+    ['Section Metadata'],
+    ['style', 'xxxl spacing'],
+  ];
+  if (bgImageElement || bgcolor) {
+    sectionMetadataCells.push(['background', bgImageElement || bgcolor]);
+  }
+  // section metadata Table
+  const sectionMetaDataTable = WebImporter.DOMUtils.createTable(
+    sectionMetadataCells,
+    document,
   );
-
-  containers.forEach(({ element, type }) => {
-    iconBlockCreation(element, type);
-  });
+  sectionMetaDataTable.classList.add('import-table');
+  block.replaceWith(table, sectionMetaDataTable);
 };
 
 const expressIconBlock = (block, document) => {
